b9e5e1eb3adf22b07c6860eb5125f43c760d411d
Kevin Jiménez <ji.oso.kev@gmail.com>
2021-02-20 21:19:18 -0600

    Correcion de la ruta en el logo

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
         <div class="Header-main">
             <div class="Header-logo">
                 <h1>
-                    <a href="/">
+                    <a href="https://kevjim.github.io/PelisTendencia/">
                         PelisTendencia
                     </a>
                 </h1>
